e71b0dbcad2be5a018a07271fce92012ff923563
Charder001 <rick.chardet@hva.nl>
2019-06-06 14:33:04 +0200

    index updated for mongo atlas support

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var find = require("array-find")
 var mongojs = require("mongojs")
 var mongoose = require('mongoose')
 var PORT = process.env.PORT
+var MONGODB_URI = process.env.MONGODB_URI
 
 //linking MongoJS to MongoDB Database called "MotoMatch" with the collection "users" 
 var db = mongojs("MotoMatch", ["users"])
